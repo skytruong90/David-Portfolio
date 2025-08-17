e2bdd430dd2fe15c431c61909b290347cf24a63c
david tan <david@David-Tan-2.local>
2025-08-17 12:29:27 -0600

    Fix Vite base path for GH Pages

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/David-Portfolio",
+  base: '/David-Portfolio/', // 👈 MUST end with slash
 })
+
+
